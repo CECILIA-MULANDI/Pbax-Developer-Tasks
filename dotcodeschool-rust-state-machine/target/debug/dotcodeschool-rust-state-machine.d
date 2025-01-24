04c695ef2defc083c19c6bc9d71b1ff0621f57f4
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-01-24 10:27:58 +0300

    removing target folder

--- dotcodeschool-rust-state-machine/target/debug/dotcodeschool-rust-state-machine.d
+++
@@ -1,1 +0,0 @@
-/home/cecilia/Developer/POLKADOT/dotcodeschool-rust-state-machine/target/debug/dotcodeschool-rust-state-machine: /home/cecilia/Developer/POLKADOT/dotcodeschool-rust-state-machine/src/balances.rs /home/cecilia/Developer/POLKADOT/dotcodeschool-rust-state-machine/src/main.rs
