c9c25b87b44f5175ccf5f3f3e10b421fddb76bb0
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-02-03 19:26:15 +0300

    Make Balances Pallet Generic

--- dotcodeschool-rust-state-machine/src/balances.rs
+++ dotcodeschool-rust-state-machine/src/balances.rs
@@ -1,42 +1,41 @@
+use num::traits::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
 
-/*
-	TODO: Define the common types used in this pallet:
-		- `AccountID`
-		- `Balance`
-
-	Then update this pallet to use these common types.
-*/
-type AccountID = String;
-type Balance = u128;
+// type AccountID = String;
+// type Balance = u128;
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountID, Balance> {
 	balances: BTreeMap<AccountID, Balance>,
 }
 
-impl Pallet {
+impl<AccountID, Balance> Pallet<AccountID, Balance>
+where
+	AccountID: Ord + Clone,
+	Balance: Zero + CheckedSub + CheckedAdd + Copy,
+{
 	pub fn new() -> Self {
 		Self { balances: BTreeMap::new() }
 	}
-	pub fn set_balance(&mut self, who: &String, amount: u128) {
+	pub fn set_balance(&mut self, who: &AccountID, amount: Balance) {
 		self.balances.insert(who.clone(), amount);
 	}
-	pub fn balance(&self, who: &String) -> u128 {
-		*self.balances.get(who).unwrap_or(&0)
+
+	pub fn balance(&self, who: &AccountID) -> Balance {
+		*self.balances.get(who).unwrap_or(&Balance::zero())
 	}
 	pub fn transfer(
 		&mut self,
-		caller: String,
-		to: String,
-		amount: u128,
+		caller: AccountID,
+		to: AccountID,
+		amount: Balance,
 	) -> Result<(), &'static str> {
 		// get balance of caller & to
 		let caller_balance = self.balance(&caller);
 		let to_balance = self.balance(&to);
 		let new_caller_balance = caller_balance
-			.checked_sub(amount)
+			.checked_sub(&amount)
 			.ok_or("You do not have enough funds to do the transfer")?;
-		let new_to_balance = to_balance.checked_add(amount).ok_or("An overflow occurred")?;
+		let new_to_balance = to_balance.checked_add(&amount).ok_or("An overflow occurred")?;
 		self.set_balance(&caller, new_caller_balance);
 		self.set_balance(&to, new_to_balance);
 		Ok(())
@@ -47,7 +46,7 @@ impl Pallet {
 mod tests {
 	#[test]
 	fn init_balances() {
-		let mut balances = super::Pallet::new();
+		let mut balances = super::Pallet::<String, u128>::new();
 		assert_eq!(balances.balance(&"alice".to_string()), 0);
 		balances.set_balance(&"alice".to_string(), 100);
 		assert_eq!(balances.balance(&"alice".to_string()), 100);
--- dotcodeschool-rust-state-machine/src/main.rs
+++ dotcodeschool-rust-state-machine/src/main.rs
@@ -1,43 +1,22 @@
 mod balances;
 mod system;
+mod types {
+	pub type AccountID = String;
+	pub type Balance = u128;
+}
 #[derive(Debug)]
 pub struct Runtime {
-	/* TODO:
-		- Create a field `system` which is of type `system::Pallet`.
-		- Create a field `balances` which is of type `balances::Pallet`.
-
-	*/
 	system: system::Pallet,
-	balances: balances::Pallet,
+	balances: balances::Pallet<types::AccountID, types::Balance>,
 }
 
 impl Runtime {
 	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
 	fn new() -> Self {
-		/* TODO: Create a new `Runtime` by creating new instances of `system` and `balances`. */
 		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
 	}
 }
 fn main() {
-	/* TODO: Create a mutable variable `runtime`, which is a new instance of `Runtime`. */
-	/* TODO: Set the balance of `alice` to 100, allowing us to execute other transactions. */
-
-	// start emulating a block
-	/* TODO: Increment the block number in system. */
-	/* TODO: Assert the block number is what we expect. */
-
-	// first transaction
-	/* TODO: Increment the nonce of `alice`. */
-	/* TODO: Execute a transfer from `alice` to `bob` for 30 tokens.
-		- The transfer _could_ return an error. We should use `map_err` to print
-		  the error if there is one.
-		- We should capture the result of the transfer in an unused variable like `_res`.
-	*/
-
-	// second transaction
-	/* TODO: Increment the nonce of `alice` again. */
-	/* TODO: Execute another balance transfer, this time from `alice` to `charlie` for 20. */
-
 	// creating a sample runtime
 	let mut runtime = Runtime::new();
 	let alice = "Alice".to_string();
