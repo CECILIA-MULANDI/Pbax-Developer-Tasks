fce86f682357db675aa9dbb0519c45f8b81e3f8c
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-06-23 21:26:05 +0300

    rc/refcell

--- ownership_/src/main.rs
+++ ownership_/src/main.rs
@@ -28,15 +28,33 @@ impl FileSystemNode {
         child: Rc<RefCell<FileSystemNode>>,
     ) -> Result<(), &'static str> {
         match self {
-            FileSystemNode::Directory { children, .. } => children.insert(name, child);
-            Ok(())
+            FileSystemNode::Directory { children, .. } => {
+                children.insert(name, child);
+                Ok(())
+            }
+            FileSystemNode::File { .. } => Err("Cannot add child to a file"),
         }
-        FileSystemNode::File{...} => Err("Cannot add child to a file"),
     }
 }
 fn main() {
-    let test_file = FileSystemNode::new_file("test.txt".to_string(), 1024);
+    let test_file = Rc::new(RefCell::new(FileSystemNode::new_file(
+        "test.txt".to_string(),
+        1024,
+    )));
     println!("Test file is: {:?}", test_file);
-    let root = FileSystemNode::new_directory("root".to_string());
+    let root = Rc::new(RefCell::new(FileSystemNode::new_directory(
+        "root".to_string(),
+    )));
     println!("The current directory is: {:?}", root);
+    let dir = root
+        .borrow_mut()
+        .add_child("home".to_string(), test_file.clone());
+    match dir {
+        Ok(_) => println!("Added file to directory successfully."),
+        Err(e) => println!("Error adding file to directory: {}", e),
+    }
+    println!("Final structure: {:#?}", root);
+    if let FileSystemNode::File { name, size } = &*test_file.borrow() {
+        println!("The name of the file is :{name} and its size is: {size} bytes");
+    }
 }
